7098be47f450fdf1e84a5e6677f7962b0fb577c7
daniil <nesterd85@gmail.com>
2017-03-06 17:10:30 +0700

    reactJS test

---
+++ WebApplication1/WebApplication1/scripts/React/Index.jsx
@@ -0,0 +1,69 @@
+﻿
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//var Persons =
+//    [
+//        { name: 'Коля' },
+//        { name: 'Петя' },
+//        { name: 'Саша' },
+//        { name: 'Вася' }
+
+//    ];
+
+
+
+class PersonList extends React.Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            items:
+                [
+                   { name: 'Коля' },
+                   { name: 'Петя' },
+                   { name: 'Саша' },
+                   { name: 'Вася' }
+                ],
+            addingField : ''
+        };
+        this.handleAddingFieldChange = this.handleAddingFieldChange.bind(this)
+    }
+
+    handleDelete(index) {
+        var items = this.state.items
+        items.splice(index, 1)
+        console.log(items)
+        this.setState({ items: items })
+    }
+
+    handleAdd() {
+        var arr = this.state.items
+        arr.splice(arr.length, 0, { name: this.state.addingField })
+        this.setState({ items: arr })
+        document.getElementById("adding-field").value = ''
+        
+        
+    }
+
+    handleAddingFieldChange(event) {
+        console.log(event.target.value)
+        this.setState({ addingField: event.target.value })
+    }
+
+    render() {
+        return <div>
+    <ul>
+        {this.state.items.map((item, index) => <li>{item.name} <input type="button" value="Удалить" onClick={this.handleDelete.bind(this, index) }/></li>)}
+    </ul>
+    <input type="text" id="adding-field" onChange={this.handleAddingFieldChange} />     <input type="button" value="Добавить" onClick={this.handleAdd.bind(this)} />
+    </div>
+    
+    }
+}
+
+ReactDOM.render(
+    <PersonList name="List" />,
+    document.getElementById("root")
+);
+
---
+++ WebApplication1/WebApplication1/scripts/React/webpack.config.js
@@ -0,0 +1,40 @@
+﻿'use strict'
+var path = require('path');
+var webpack = require("webpack/lib/webpack")
+var ManifestPlugin = require('webpack-manifest-plugin');
+var ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
+var WebpackMd5Hash = require('webpack-md5-hash');
+module.exports = {
+    entry: {
+        'index': './index.jsx'
+    },
+    output: {
+        path: "./Release",
+        filename: "[name].js"
+    },
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    module: {
+        loaders: [{
+            test: /\.jsx$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        },
+        {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }]
+    },
+
+    devtool: "source-map",
+
+    plugins: [
+         new webpack.optimize.CommonsChunkPlugin({ name: "vendor" })
+    ]
+}
